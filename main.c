3c1023f7f13f6858dd2848ec2ef1ef8603e5c7f2
Scott Munro <scottnmunro@gmail.com>
2017-08-03 16:05:33 -0700

    parse_frac working for non-mixed rational numbers

--- main.c
+++ main.c
@@ -3,10 +3,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct
+{
+    int numer;
+    int denom;
+} frac;
+
+typedef struct
+{
+    int whole_part;
+    frac frac_part;
+} mixed_frac;
+
 void error(const char* error_str)
 {
     printf("%s\n", error_str);
     exit(1);
+}
+
+frac parse_frac(char* frac_str)
+{
+    frac ret_val = {1, 1};
+
+    char* frac_split = strchr(frac_str, '_');
+    // not a mixed fraction
+    if (frac_split == NULL)
+    {
+        /*
+         In particular, if *nptr is not '\0' but **endptr is '\0' on
+                    return, the entire string is valid
+        */
+        char* frac_divider;
+        int numerator = strtol(frac_str, &frac_divider, 10);
+
+        // if strtol didn't find an invalid character than the frac_str is just
+        // a whole number
+        if (*frac_divider == '\0')
+        {
+            return (frac){ numerator, 1 };
+        }
+
+        int denominator = strtol(frac_divider + 1, NULL, 10);
+        return (frac){ numerator, denominator };
+    }
+
+    return ret_val;
 }
 
 int main(int argc, char** argv)
@@ -16,13 +57,26 @@ int main(int argc, char** argv)
         error("Not enough arguments\n");
     }
 
-    char* input_str = argv[1];
+    char* expression_str = argv[1];
     char* save_ptr;
 
-    // fetch the arguments from the string
+    // split the arguments from input string into tokens
     // char *strtok_r(char *str, const char *delim, char **saveptr);
-    char* op_1_str = strtok_r(input_str, " ", &save_ptr);
+    char* op_1_str = strtok_r(expression_str, " ", &save_ptr);
     char* operator_str = strtok_r(NULL, " ", &save_ptr);
     char* op_2_str = strtok_r(NULL, " ", &save_ptr);
-    printf("1: %s\n2: %s\n3: %s\n", op_1_str, operator_str, op_2_str);
+
+    // turn tokens into fraction structs
+    // change so both fractions have same denom
+    // perform math on fractions
+    // reduce
+    //      - divide out whole numbers until num is smaller than denom
+    //      - save as whole num
+    //      - reduce fractional part
+    //      - print out result
+
+    frac res = parse_frac(op_1_str);
+    frac res2 = parse_frac(op_2_str);
+    printf(" %d \n---\n %d \n\n", res.numer, res.denom);
+    printf(" %d \n---\n %d \n", res2.numer, res2.denom);
 }
