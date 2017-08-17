c74cb5d43caca8dc0c7784f0d6bfa6a9a19a048b
Scott Munro <scottnmunro@gmail.com>
2017-08-16 20:52:11 -0700

    updating c template for new helpers file

--- .c_template.c
+++ .c_template.c
@@ -2,12 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void error(const char* error_str)
-{
-    printf("%s\n", error_str);
-    exit(1);
-}
-
-int main(int argc, char** argv)
+int main(
+    int argc,
+    char** argv
+    )
 {
 }
