ba28d34a3a25c5a4631cf0088c79cb4e1ba53054
Scott Munro <scottnmunro@gmail.com>
2017-08-19 14:51:14 -0700

    wrote a bunch of err tests

--- test/test_runner.py
+++ test/test_runner.py
@@ -2,35 +2,65 @@ import subprocess
 from tests import test_set
 
 class bc:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    _HEADER = '\033[95m'
+    _OKBLUE = '\033[94m'
+    _OKGREEN = '\033[92m'
+    _WARNING = '\033[93m'
+    _FAIL = '\033[91m'
+    _ENDC = '\033[0m'
+    _BOLD = '\033[1m'
+    _UNDERLINE = '\033[4m'
+
+    @staticmethod
+    def HEADER(s):
+        return bc._HEADER + str(s) + bc._ENDC
+
+    @staticmethod
+    def OKBLUE(s):
+        return bc._OKBLUE + str(s) + bc._ENDC
+
+    @staticmethod
+    def OKGREEN(s):
+        return bc._OKGREEN + str(s) + bc._ENDC
+
+    @staticmethod
+    def WARNING(s):
+        return bc._WARNING + str(s) + bc._ENDC
+
+    @staticmethod
+    def FAIL(s):
+        return bc._FAIL + str(s) + bc._ENDC
+
+    @staticmethod
+    def BOLD(s):
+        return bc._BOLD + str(s) + bc._ENDC
+
+    @staticmethod
+    def UNDERLINE(s):
+        return bc._UNDERLINE + str(s) + bc._ENDC
 
 def main():
     for test_file, expected_output, returncode in test_set:
         completed_test = subprocess.run(["./main", test_file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
         success = True
-        output = test_file + ": "
+        output = bc.HEADER(test_file) + ": "
         failed_to_match_output = expected_output not in completed_test.stdout
         failed_to_match_retcode = returncode != completed_test.returncode
         if not (failed_to_match_output or failed_to_match_retcode):
-            output += bc.OKGREEN + bc.BOLD + "SUCCESS" + bc.ENDC
+            output += bc.OKGREEN(bc.BOLD("SUCCESS"))
         else:
-            output += bc.FAIL + bc.BOLD + "FAIL" + bc.ENDC
+            output += bc.FAIL(bc.BOLD("FAIL"))
             if failed_to_match_output:
                 success = False
-                output += "\nexpected output not found in output"
+                output += "\nexpected output {}\nnot found in output {}"\
+                            .format(bc.WARNING(expected_output),
+                                    bc.WARNING(completed_test.stdout))
             if failed_to_match_retcode:
                 success = False
                 output += "\nreturn code expected {}, return code received {}"\
-                            .format(bc.WARNING + str(returncode) + bc.ENDC,
-                                    bc.WARNING + str(completed_test.returncode) + bc.ENDC)
-        output += "\n\n"
+                            .format(bc.WARNING(returncode),
+                                    bc.WARNING(completed_test.returncode))
+        output += "\n"
         print(output)
 
 
--- test/tests.py
+++ test/tests.py
@@ -1,3 +1,9 @@
-test_set = [   #    json source     |      expected output     |     return code
-               ("test/jsons/err1.json", "Error, bad obj syntax", 1)
+test_set = [
+               ("test/jsons/err_improper_obj_opening.json", "Error, bad obj syntax", 1),
+               ("test/jsons/err_key_missing_opening_quote.json", "When trying to parse a key, a value other than an opening quote was found", 1),
+               ("test/jsons/err_key_missing_colon.json", "Missing ':' after key", 1),
+               ("test/jsons/err_unexpected_EOF.json", "Unexpected EOF", 1),
+               ("test/jsons/err_improper_num.json", "Invalid number could not be parsed", 1),
+               ("test/jsons/err_invalid_lookahead.json", "invalid character parsed", 1),
+               ("test/jsons/err_keyvalue_pair_unclosed.json", "End of keyvalue pair was not followed by a comma or a closing brace", 1)
            ];
