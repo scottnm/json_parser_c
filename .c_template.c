4faff2cafb31553f2e7a754ff078c28f36d05b47
Scott Munro <scottnmunro@gmail.com>
2017-08-19 12:38:23 -0700

    separating out json project from c_prac, deleting cruft

--- .c_template.c
+++
@@ -1,9 +0,0 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "helpers.h"
-
-int main(int argc, char** argv)
-{
-}
--- archive.sh
+++
@@ -1,9 +0,0 @@
-ARCHIVEDIR=".archive"
-FILENAME="$ARCHIVEDIR/$(date +%F).txt"
-
-if [ -f $FILENAME ]; then
-    echo "file with that name already exists. archive manually"
-else
-    mv main.c $FILENAME
-    echo "archived! $FILENAME"
-fi
